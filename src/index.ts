aa800454e35fa9c12249e9625245b51c30d47879
HaiVQ <me@haivq.com>
2024-06-03 22:13:11 +0700

    remove test params

--- src/index.ts
+++ src/index.ts
@@ -24,15 +24,14 @@ export default {
 		const response = await fetch(signed, {
 			cf: {
 				image: {
-					brightness: 0.1,
 					draw: [{
-						url: 'https://picsum.photos/id/237/200',
-						bottom: 5, // 5 pixels from the bottom edge
-						right: 5, // 5 pixels from the right edge
-						fit: 'contain', // make it fit within 100x50 area
-						width: 200,
-						height: 200,
-						opacity: 0.8, // 20% transparent
+						url: 'https://picsum.photos/id/237/100',
+						bottom: 5,
+						right: 5,
+						fit: 'contain',
+						width: 100,
+						height: 100,
+						opacity: 0.4,
 					}]
 				}
 			}
