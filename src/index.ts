382066c9f72505ae9a800981a406104ca1380ce5
HaiVQ <me@haivq.com>
2024-06-03 22:08:49 +0700

    set permanent sample photo

--- src/index.ts
+++ src/index.ts
@@ -26,8 +26,13 @@ export default {
 				image: {
 					brightness: 0.1,
 					draw: [{
-						url: 'https://picsum.photos/300',
-						repeat: true,
+						url: 'https://picsum.photos/id/237/200',
+						bottom: 5, // 5 pixels from the bottom edge
+						right: 5, // 5 pixels from the right edge
+						fit: 'contain', // make it fit within 100x50 area
+						width: 200,
+						height: 200,
+						opacity: 0.8, // 20% transparent
 					}]
 				}
 			}
